03b19e547981b95e44bcd8d04522e7829c3baaa0
Yogesh <pantyogesh992@gmail.com>
2026-07-05 10:08:04 +0530

    JavaScript

---
+++ JS/hello.js
@@ -0,0 +1,17 @@
+function printChai(){
+    console.log("Hello Chai");
+    
+}
+
+function brush(kitne){
+    console.log(`Hanji, bring ${kitne} brush `);
+}
+
+function addTwo(num1, num2){
+    return num1 + num2
+}
+brush(4);
+printChai()
+addTwo(1,1)
+console.log(addTwo(2,2));
+
