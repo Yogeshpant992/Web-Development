f59c753460486420ae832601097b8298058eb1a6
Yogesh <pantyogesh992@gmail.com>
2026-07-08 18:46:01 +0530

    pizza file

---
+++ JS/pizza.js
@@ -0,0 +1,11 @@
+let numberofGuest = -1;
+let pizzaSize ;
+if(numberofGuest<=2){
+    console.log("small")
+}
+else if(numberofGuest <=5){
+    console.log("medium")
+}
+else {
+    console.log("Large")
+}
