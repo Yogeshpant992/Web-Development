eef65ac25e736156096c4628d264948fd739ff9c
Yogesh <pantyogesh992@gmail.com>
2026-07-10 09:49:44 +0530

    javaScript file

---
+++ JS/outft.js
@@ -0,0 +1,13 @@
+// server call --> rainy , cloudy , sunny 
+
+let weather = "cloudy"
+if (weather === "rainy") {
+    console.log("It is rainy , umberalla bring");
+}
+else if(weather ==="cloudy"){
+    console.log("Jacket le ja bhai")
+}
+else {
+    console.log("else parth ji ");
+}
+
