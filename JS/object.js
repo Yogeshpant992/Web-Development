8bf57d44be66510743ac6d0b2c6261ff9bc1beff
Yogesh <pantyogesh992@gmail.com>
2026-07-10 09:50:34 +0530

    javaScript object

---
+++ JS/object.js
@@ -0,0 +1,28 @@
+const person = {
+    x : 10,
+    firstName: 'Yogesh',
+    SecondName: 'Pant',
+    hobbies:['Coding','Reading'],
+    isMarried: false,
+    address: {
+        hno: 1,
+        street: 1,
+        countryCode: 'IN'
+    }
+}
+
+
+console.log(person.address.street)
+
+const remote = {
+    color: 'black',
+    brand: 'XYZ',
+    dimensions: { height: 1, width: 1},
+    turnOff : function(){
+
+    },
+    voulmeUp: function(){
+        
+    }
+    }
+ 
