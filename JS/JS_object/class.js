0af41028e06fbb220d3aa08eb655a32beceb8a27
Yogesh <pantyogesh992@gmail.com>
2026-07-20 08:18:11 +0530

    class file

--- JS/JS_object/class.js
+++ JS/JS_object/class.js
@@ -1,1 +1,16 @@
+class Person {
+    constructor(fname, lname){
+        this.fname = fname;
+        this.lname = lname;
+    }
 
+    getfullname(){
+        return `${this.fname} ${this.lname}`;
+    }
+}
+
+const p1 = new Person('Rakesh','Bedi');
+const p2 = new Person('Akash','Kadlag');
+
+console.log(p1.getfullname());
+console.log(p2.getfullname()); 
