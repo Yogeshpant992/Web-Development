3b4550334ff5938c1e8fcfac1035d65f107b0d6f
Yogesh <pantyogesh992@gmail.com>
2026-07-23 20:39:41 +0530

    js file

--- JS/Add_two_no/first.js
+++ JS/Add_two_no/first.js
@@ -1,1 +1,22 @@
+const button = document.querySelector('button');
+button.addEventListener('click',()=>{
+    // Read the data
+    const first_number = document.getElementById('first');
+    const number1 = Number(first_number.value);
 
+    const second_number = document.getElementById('second');
+    const number2 = Number(second_number.value);
+    
+    if(isNaN(number1)|| isNaN(number2)){
+        return ;
+    }
+    // output the result
+
+    const result = number1+number2;
+    const re = document.getElementById('result');
+    re.textContent = "Result:"+ result;
+
+})
+
+//BMI calculator
+//Dowry calculator 
