435e159f1747522e46dcb200db7c290a073b33c2
Yogesh <pantyogesh992@gmail.com>
2026-07-31 22:00:16 +0530

    js file

--- JS/Forms_in_JS/first.js
+++ JS/Forms_in_JS/first.js
@@ -1,1 +1,56 @@
+const form = document.querySelector('form');
 
+// form.addEventListener('input',(event)=>{
+//     console.log(event.target.value);
+// })
+
+
+// form.addEventListener('change',(event)=>{
+//     console.log(event.target.value);
+// })
+
+// form.addEventListener('focusout',(event)=>{
+//     console.log(event.target.value);
+// })
+
+
+// form.addEventListener('focusout',(event)=>{
+//     console.log(event.target.value);
+// })
+
+// Whenever there is change in forms
+
+// form.addEventListener('submit',(event)=>{
+//     console.log("form Submitted");
+// })
+
+// form.addEventListener('reset',(event)=>{
+//     console.log("form reset");
+// })
+
+
+// form.addEventListener('submit',(event)=>{
+//     event.preventDefault(); //   it help to prevent page from getting refreshed fully or prevent current or default behaviour.
+//     const first = document.getElementById("first");
+//     console.log(first.value);
+
+//     const second = document.getElementById("second");
+//     console.log(second.value);
+
+//     const third = document.getElementById("third");
+//     console.log(third.value);
+
+//     const result = document.getElementById('result');
+//     result.innerText = `${first.value} ${second.value} is a good Boy`;
+//     document.body.append(result);
+// })
+
+form.addEventListener('submit',(event)=>{
+ const data = new FormData(form);
+    event.preventDefault();
+    for(let [key,value] of data.entries())
+        console.log(key ,value);
+
+    // console.log(Array.from(data.keys()));
+    // console.log(Array.from(data.values()));
+})
